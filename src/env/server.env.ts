45f413e36bb6558dd265680a2dbe82308814bb1b
Érico <oliv.ericorib@gmail.com>
2025-07-08 21:37:38 -0300

    refactor: adicionar nas configurações de ambiente

--- src/env/server.env.ts
+++ src/env/server.env.ts
@@ -1,4 +1,4 @@
-export const SERVER_PORT = process.env.SERVER_PORT;
-export const SERVER_HOST = process.env.SERVER_HOST;
-export const SERVER_PROTOCOL = process.env.SERVER_PROTOCOL;
-export const SERVER_URL = `${process.env.SERVER_PROTOCOL}://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`;
+export const SERVER_PORT: number = +process.env.SERVER_PORT;
+export const SERVER_HOST: string = process.env.SERVER_HOST;
+export const SERVER_PROTOCOL: string = process.env.SERVER_PROTOCOL;
+export const SERVER_URL: string = `${process.env.SERVER_PROTOCOL}://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`;
